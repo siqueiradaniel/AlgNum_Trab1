908be172855ead721d77bd911962f6711a2d82f5
siqueiradaniel <danielsiqueira111@gmail.com>
2023-05-22 13:47:16 -0300

    Plot_3.1 com legenda

--- SolveLinearFunction.m
+++ SolveLinearFunction.m
@@ -1,4 +1,6 @@
 function SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax)
+  strLeg = 'V(t)';
+  
   if Qin < Qout
       solve = fzero(@(t) yx(Qin, Qout, V0, t, t0), t0);
       fprintf('Caso a) Qin == %.2f, Qout == %.2f\nEvento excepcional em tempo = %.2f\n', Qin, Qout, solve);
@@ -10,6 +12,7 @@ function SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax)
   else
       fprintf('Caso c) Qin == %.2f, Qout == %.2f\nConstancia de volume: Qin = Qout\n', Qin, Qout);
       Xsup = 500;
+      strLeg = sprintf('V(t)=V0=%.2f L', V0);
   endif
   endif
 
@@ -17,11 +20,25 @@ function SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax)
   
   t = t0 : 1 : 1000;
   fun = @(t) 500;
+  fontsize = 16;
+  leg = {};
 
   figure;
   hold on;
 
-  plot(t, yx(Qin, Qout, V0, t, t0));
+  plot(t, yx(0, 0, V0, t, 0), 'b--');
+  plot(t, yx(Qin, Qout, V0, t, t0), 'r-');
+  plot(t, yx(0, 0, Vmax, t, 0), 'c--');
+  
+  leg{end+1} = sprintf('V0=%.2f L', V0);
+  leg{end+1} = sprintf(strLeg);
+  leg{end+1} = sprintf('Vmax=5000.00 L', Vmax);
+  
+  # Apresenta legenda
+  h = legend(leg);
+  set (h, 'fontsize', fontsize, 'location', 'east');
+  hold off;
+  shg;
   
   axis(ax);
   
