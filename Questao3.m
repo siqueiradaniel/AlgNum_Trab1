8a7007bfa6d60ccc2fb5562f22e2acd5ca2b83fe
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2023-05-22 10:03:23 -0300

    Questao 3.1

--- Questao3.m
+++ Questao3.m
@@ -1,40 +1,38 @@
-pkg load symbolic;
+function Questao3()
+  pkg load symbolic;
 
-## Q_1
-syms V(t) t0 V0 Qin Qout
-deltaQ = Qin - Qout;
-edo = diff(V(t), t) == deltaQ;
-cond = V(t0) == V0;
+  ## Q_1
+  syms V(t) t0 V0 Qin Qout
+  deltaQ = Qin - Qout;
+  edo = diff(V(t), t) == deltaQ;
+  cond = V(t0) == V0;
 
-fprintf('Solucao:\n');
-sol = dsolve(edo, cond)
+  fprintf('Solucao:\n');
+  sol = dsolve(edo, cond)
 
-## Q_2
-fprintf('\nVersao numerica da solucao:\n');
-yx = matlabFunction(sol)
+  ## Q_2
+  fprintf('\nVersao numerica da solucao:\n');
+  yx = matlabFunction(sol)
 
-## Q_3
-Vmax = 5000;  # Volume máximo
-t0 = 0;
-V0 = 2000;    # Volume inicial
+  ## Q_3
+  Vmax = 5000;  # Volume máximo
+  t0 = 0;       # Valor inicial de t
+  V0 = 2000;    # Volume inicial
 
-# a) Qin < Qout
-Qin = 100;    # Taxa de entrada
-Qout = 200;   # Taxa de saída
+  # a) Qin < Qout
+  Qin = 100;    # Taxa de entrada
+  Qout = 200;   # Taxa de saída
+  SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax);
 
-tempo = fzero(@(t) yx(Qin, Qout, V0, t, t0), [t0, 100]);
-fprintf('Caso a) Qin == %.2f, Qout == %.2f\nEvento excepcional em tempo = %.2f\n', Qin, Qout, tempo);
+  # b) Qin > Qout
+  Qin = 300;    # Taxa de entrada
+  Qout = 200;   # Taxa de saída
+  SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax);
 
-# b) Qin > Qout
-Qin = 300;    # Taxa de entrada
-Qout = 200;   # Taxa de saída
 
-fprintf('Caso b) Qin == %.2f, Qout == %.2f\nEvento excepcional em ', Qin, Qout);
-tempo = solve(subs(sol) == Vmax, t)
-# Não consegui usar a funcao fsolve.
+  # c) Qin == Qout
+  Qin = 100;    # Taxa de entrada
+  Qout = 100;   # Taxa de saída
+  SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax);
 
-# c) Qin == Qout
-Qin = 100;    # Taxa de entrada
-Qout = 100;   # Taxa de saída
 
-fprintf('Caso c) Qin == %.2f, Qout == %.2f\nConstancia de volume: Qin = Qout\n', Qin, Qout);
---
+++ SolveLinearFunction.m
@@ -0,0 +1,36 @@
+function SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax)
+  ax = [t0-1 100 0 5000];
+  
+  edostr = "title";
+  figure;
+  hold on;
+  markersize = 50;
+  lw = 1.5;
+  fontsize = 16;
+  leg = {};
+
+  plot(t, yx(t), 'k', 'linewidth', lw)
+  #plot(Vmax);
+  #plot(t0);
+  
+  # Legenda
+  leg{end+1} = 'V(t)';
+  xlabel ("t");
+  ylabel ("V(t)");
+  title(edostr, 'fontsize', fontsize);
+
+  axis(ax);
+  
+  if Qin < Qout
+      solve = fzero(@(t) yx(Qin, Qout, V0, t, t0), t0);
+      fprintf('Caso a) Qin == %.2f, Qout == %.2f\nEvento excepcional em tempo = %.2f\n', Qin, Qout, solve);
+      
+  else if Qin > Qout
+      V0 = V0 - Vmax;
+      solve = fzero(@(t) yx(Qin, Qout, V0, t, t0), t0);
+      fprintf('Caso b) Qin == %.2f, Qout == %.2f\nEvento excepcional em %.2f\n', Qin, Qout, solve);
+  else 
+      fprintf('Caso c) Qin == %.2f, Qout == %.2f\nConstancia de volume: Qin = Qout\n', Qin, Qout);
+  endif
+  endif
+ 
