a90cc7bf0e240f271883abec4d7b2dd190dbe7a8
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2023-05-21 23:31:07 -0300

    Questao 2 Finalizada

--- T1_AlgNum.m
+++ T1_AlgNum.m
@@ -15,10 +15,13 @@ pkg load symbolic;
 # Variável simbólica
 syms y(x);
 
+# Desabilita os Warnings
+# warning('off');
+
 function [] = solveEDO(f, edostr, x0, y0, h, n, ode)
   syms y(x);
 
-  ax = [x0 x0+h*(n) 0 1];      # Margem limite plot [x0 xf y0 yf]
+  ax = [x0 x0+h*(n)];      # Margem limite plot [x0 xf y0 yf]
 
   fprintf('EDO:\n');
   ode
@@ -32,7 +35,7 @@ function [] = solveEDO(f, edostr, x0, y0, h, n, ode)
   x = x0 : h/10 : x0+h*(n-1);
   X = x0 : h    : x0+h*(n-1);
 
-  clf
+  figure;
   hold on;
   markersize = 50;
   lw = 1.5;
@@ -125,13 +128,41 @@ function [] = solveEDO(f, edostr, x0, y0, h, n, ode)
   # Aproximacao e pontos
   plot (X, Y, cols{8});
   leg{end+1} = sprintf('DP - Passo Adaptativo');
+  adaptX = X;
+  adaptY = Y;
 
   # Apresenta legenda
   h = legend(leg);
   set (h, 'fontsize', fontsize, 'location', 'east');
   hold off;
-
   shg;
+
+  figure;
+  title(strcat("Erros - Escala Logaritmica --- ", edostr), 'fontsize', fontsize);
+  hold on;
+  markersize = 50;
+  lw = 1.5;
+  fontsize = 16;
+  leg = {};
+  leg{end+1} = sprintf('Euler');
+  leg{end+1} = sprintf('Euler Melhorado');
+  leg{end+1} = sprintf('Euler Modificado');
+  leg{end+1} = sprintf('Van der Houwen');
+  leg{end+1} = sprintf('Ralston');
+  leg{end+1} = sprintf('DP - Butcher Tableau');
+  leg{end+1} = sprintf('DP - Passo Fixo');
+  leg{end+1} = sprintf('DP - Passo Adaptativo');
+
+  for i=1:numMetodos
+    axisX = eixoX;
+    axisY = erros(:,i);
+    semilogy(axisX, axisY, cols{i});
+  endfor
+    semilogy(adaptX, adaptY, cols{8});
+
+  h = legend(leg);
+  set (h, 'fontsize', fontsize, 'location', 'east');
+  hold off;
 
   #Gerando a Tabela
   fprintf('%10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s\n', 'x', 'V. Exato', 'Euler', 'Euler Mel', 'Euler Mod', 'VdHouven/W', 'Ralston', 'DormPrinc', 'ODE45 Fixo');
@@ -148,19 +179,33 @@ function [] = solveEDO(f, edostr, x0, y0, h, n, ode)
 endfunction
 
 
-##### Letra a)
+fprintf("##### Letra A #####\n");
 f = @(x,y) 1.0 / exp(x) - 2.0*y;
-edostr = 'EDO: dy/dx = 1/e^x - 2y ; Valor inicial: y(0)=1 --- Solução: y(x) = exp(-x)';
+edostr = 'PVI: dy/dx = 1/e^x - 2y, y(0)=1 --- Solução: y(x) = exp(-x)';
 x0 = 0; y0 = 1; h = 0.5; n = 5;
 ode = exp(x) * diff(y, x) + 2*exp(x) * y == 1;
-
 solveEDO(f, edostr, x0, y0, h, n, ode);
 
 
-##### Letra b)
+fprintf("##### Letra B #####\n");
 f = @(x,y) sin(x)/x^3 - 3*y/x;
-edostr = 'EDO: dy/dx = sin(x)/x^3 - 3y/x ; Valor inicial: y(pi)=1 --- Solução: (-cos (x) - 1 + pi^3) / x^3';
+edostr = 'PVI: dy/dx = sin(x)/x^3 - 3y/x, y(pi)=1 --- Solução: (-cos (x) - 1 + pi^3) / x^3';
 x0 = pi; y0 = 1; h = 1.0; n = 5;
-ode = x*diff(y)+3*y == sin(x)/x^2;
+ode = x*diff(y, x)+3*y == sin(x)/x^2;
+solveEDO(f, edostr, x0, y0, h, n, ode);
 
+
+fprintf("##### Letra C #####\n");
+f = @(x,y) cos(x)^2-tan(x)*y;
+edostr = 'PVI: dy/dx = cos(x)^2-tan(x)y, y(pi/8)=1 --- Solução: (sin(x)- 3*sqrt(2)*sqrt(sqrt(2)+2)/2+5*sqrt(sqrt(2)+2)/2)*cos(x)';
+x0 = pi/8; y0 = 1; h = pi/16; n = 5;
+ode = diff(y, x)+tan(x)*y == cos(x)^2;
+solveEDO(f, edostr, x0, y0, h, n, ode);
+
+
+fprintf("##### Letra D #####\n");
+f = @(x,y) (1 - 1/x - 2*y)/x;
+edostr = 'PVI: dy/dx = (1 - 1/x - 2*y)/x, y(1)=1 --- Solução: (x^2/2-x+3/2)/x^2';
+x0 = 1; y0 = 1; h = 0.1; n = 5;
+ode = x*diff(y, x) + 2*y == 1-1/x;
 solveEDO(f, edostr, x0, y0, h, n, ode);
