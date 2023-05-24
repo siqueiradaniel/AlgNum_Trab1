dfe8187c184cd7cbca7d223db9e0bff5c63e299f
siqueiradaniel <danielsiqueira111@gmail.com>
2023-05-24 10:58:27 -0300

    Tirando comentários

--- Questao3.m
+++ Questao3.m
@@ -2,7 +2,7 @@ function Questao3()
   pkg load symbolic;
   
   ### Questao 3.1
-  
+  fprintf('Questão 3.1:\n');
   ## Q_1
   syms V(t) t0 V0 Qin Qout
   deltaQ = Qin - Qout;
@@ -38,30 +38,24 @@ function Questao3()
   SolveLinearFunction(yx, Qin, Qout, V0, t, t0, Vmax);
 
   ### Questao 3.2
-  
-  pkg load symbolic;
+  fprintf('Questão 3.2:\n');
     
   ### Q_1
   ## Determinar a função V(t)
-  syms V(t) t0 V0 Qin Qout
+  syms V(t) t0 V0 Qin Qout;
   deltaQ = Qin - Qout;
   edo = diff(V(t), t) == deltaQ;
   cond = V(t0) == V0;
   
-  fprintf('Solucao:\n');
-  #sol = dsolve(edo, cond)
-  
-  fprintf('\nVersao numerica da solucao:\n');
-  #yx = matlabFunction(sol)
+  sol = dsolve(edo, cond);
+
   
   ## Determinar a funcao c(t):
   syms c(t) Qin cin c0 V(t) m(t)
-    
   ode = diff(c, t) == Qin*(cin-c)/sol;
   cond = c(0) == c0;
-  
-  solC = dsolve(ode, cond);
-  #disp('Solução Analítica: i(t): EDO='); solC
+  fprintf("c(t) analiticamente:\n");
+  solC = dsolve(ode, cond)
   
   ### Q_2 - Montar m(t) = c(t)*V(t)
   syms m(t);
@@ -69,8 +63,12 @@ function Questao3()
   m(t) = sol*solC;
   
   ### Q_3 - Converter c(t) e m(t) em numeric function
-  fun_c = matlabFunction(solC);
-  fun_m = matlabFunction(m(t));
+  fprintf('\nVersao numerica de V(t):\n');
+  fun_V = matlabFunction(sol)
+  fprintf('\nVersao numerica de c(t):\n');
+  fun_c = matlabFunction(solC)
+  fprintf('\nVersao numerica de m(t):\n');
+  fun_m = matlabFunction(m(t))
   
   ### Q_4
   Vmax = 5000;
